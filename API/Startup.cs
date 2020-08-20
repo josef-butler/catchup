065f2f3685e1eabaaa6bd8cee449f8787196a7e3
Joe Butler <joe@joebutler.dev>
2020-08-20 20:08:22 +1200

    Add axios and basic values to App.tsx. Also add CORS policy for the React client

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,14 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                // Add an acceptance policy for and requests from localhost:3000 (eg. the client)
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -46,6 +54,13 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: [{
+        id: 1,
+        name: 'Value 101'
+      },
+      {
+        id: 2,
+        name: 'Value 202'
+      }]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
