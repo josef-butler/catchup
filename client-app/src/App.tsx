09a121a726a6b44f7fd107ca0077e89e1ef8cc28
Joe Butler <joe@joebutler.dev>
2020-08-25 20:43:46 +1200

    Confirm semantic UI working

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react"
-import axios from "axios"
+import React from 'react'
+import axios from 'axios'
 
-import logo from "./logo.svg"
-import "./App.css"
+import './App.css'
 import 'semantic-ui-css/semantic.min.css'
+
+import { Header, Icon } from 'semantic-ui-react'
 
 class App extends React.Component {
   state = {
@@ -11,7 +12,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    axios.get("http://localhost:5000/api/values/").then((response) => {
+    axios.get('http://localhost:5000/api/values/').then(response => {
       console.log(response)
       this.setState({
         values: response.data,
@@ -21,15 +22,16 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>CatchUp</Header.Content>
+        </Header>
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li key={value.id}>{value.name}</li>
+          ))}
+        </ul>
       </div>
     )
   }
