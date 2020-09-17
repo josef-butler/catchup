a7e81d8a3fb32d48ef5a7c04c241892b6c1af1b4
Joe Butler <joe@joebutler.dev>
2020-09-17 19:52:08 +1200

    Update App.tsx to use react hooks in place of being a class component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,41 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import 'semantic-ui-css/semantic.min.css'
 import { Header, Icon } from 'semantic-ui-react'
 import { IActivity } from '../models/activity'
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
+    const [activities, setActivities] = useState<IActivity[]>([])
 
-class App extends React.Component<{}, IState> {
-  readonly state: IState = {
-    activities: [],
-  }
+    useEffect(() => {
+        axios
+            .get<IActivity[]>('http://localhost:5000/api/activities/')
+            .then(response => {
+                setActivities(response.data)
+            })
+    }, [])
 
-  componentDidMount() {
-    axios.get<IActivity[]>('http://localhost:5000/api/activities/').then(response => {
-      this.setState({
-        activities: response.data,
-      })
-    })
-  }
-
-  render() {
     return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>CatchUp</Header.Content>
-        </Header>
-        <ul>
-          {this.state.activities.map((activity) => (
-            <li key={activity.id}>{activity.title}</li>
-          ))}
-        </ul>
-      </div>
+        <div>
+            <Header as='h2'>
+                <Icon name='users' />
+                <Header.Content>CatchUp</Header.Content>
+            </Header>
+            <ul>
+                {activities.map(activity => (
+                    <li key={activity.id}>{activity.title}</li>
+                ))}
+            </ul>
+        </div>
     )
-  }
 }
 
 export default App
