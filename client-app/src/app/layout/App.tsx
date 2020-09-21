5336b0d87e58df8db0f229463af3aade959a25c1
Joe Butler <joe@joebutler.dev>
2020-09-21 15:33:47 +1200

    Add basic nav bar and remove unused files

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import 'semantic-ui-css/semantic.min.css'
-import { Header, Icon } from 'semantic-ui-react'
 import { IActivity } from '../models/activity'
+import { NavBar } from '../../features/nav/NavBar'
+import { Container } from 'semantic-ui-react'
 
 const App = () => {
-    const [activities, setActivities] = useState<IActivity[]>([])
+  const [activities, setActivities] = useState<IActivity[]>([])
 
-    useEffect(() => {
-        axios
-            .get<IActivity[]>('http://localhost:5000/api/activities/')
-            .then(response => {
-                setActivities(response.data)
-            })
-    }, [])
+  useEffect(() => {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities/')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
 
-    return (
-        <div>
-            <Header as='h2'>
-                <Icon name='users' />
-                <Header.Content>CatchUp</Header.Content>
-            </Header>
-            <ul>
-                {activities.map(activity => (
-                    <li key={activity.id}>{activity.title}</li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ul>
+          {activities.map(activity => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
+      </Container>
+    </>
+  )
 }
 
 export default App
